479025d4f7b9a6b42c29078b82b86766ae126d50
oppih-ub <letwego28@gmail.com>
2011-08-24 19:45:52 +0800

    learn to deal with views

--- urls.py
+++ urls.py
@@ -13,5 +13,9 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
+    (r'^polls/$', 'polls.views.index'),
+    (r'^polls/(?P<poll_id>\d+)/$','polls.views.detail'),
+    (r'^polls/(?P<poll_id>\d+)/results/$','polls.views.results'),
+    (r'^polls/(?P<poll_id>\d+)/vote/$','polls.views.vote'),
     url(r'^admin/', include(admin.site.urls)),
 )
