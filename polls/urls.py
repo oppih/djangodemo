33658f27a7ca427452b487773eab63dc495a7795
oppih-ub <letwego28@gmail.com>
2011-08-25 19:35:16 +0800

    polls urls.py still problem

--- polls/urls.py
+++ polls/urls.py
@@ -1,11 +1,24 @@
-from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls.defaults import *
+from django.views.generic import DetailView, ListView
+from polls.models import Poll
 
 # Uncomment the next two lines to enable the admin:
 
-urlpatterns = patterns('polls.views',
-    (r'^$', 'index'),
-    (r'^(?P<poll_id>\d+)/$','detail'),
-    (r'^(?P<poll_id>\d+)/results/$','results'),
+urlpatterns = patterns('',
+    (r'^$', 
+        ListView.as_view(
+            queryset=Poll.objects.order_by('-pub_date')[:5],
+            context_object_name='latest_poll_list',
+            template_name='polls/index.html'))
+    (r'^(?P<pk>\d+)/$',
+        DetailView.as_view(
+            model=Poll,
+            template_name='polls/detail.html')),
+    url(r'^(?P<pk>\d+)/results/$',
+        Detail.as_view(
+            model=Poll,
+            template_name='polls/results.html'),
+        name='poll_results'),
     (r'^(?P<poll_id>\d+)/vote/$','vote'),
 )
 
