7294b255a2a0f2e0db8974ed0ca045f69ff29f6e
oppih-ub <letwego28@gmail.com>
2011-08-30 18:48:26 +0800

    in urls.py, I forgot some commmas

--- polls/urls.py
+++ polls/urls.py
@@ -9,16 +9,16 @@ urlpatterns = patterns('',
         ListView.as_view(
             queryset=Poll.objects.order_by('-pub_date')[:5],
             context_object_name='latest_poll_list',
-            template_name='polls/index.html'))
+            template_name='polls/index.html')),
     (r'^(?P<pk>\d+)/$',
         DetailView.as_view(
             model=Poll,
             template_name='polls/detail.html')),
     url(r'^(?P<pk>\d+)/results/$',
-        Detail.as_view(
+        DetailView.as_view(
             model=Poll,
             template_name='polls/results.html'),
         name='poll_results'),
-    (r'^(?P<poll_id>\d+)/vote/$','vote'),
+    (r'^(?P<poll_id>\d+)/vote/$','polls.views.vote'),
 )
 
